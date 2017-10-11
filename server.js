0f377a91fc7d8c9a2713e5ce8762149039affa6e
Jeremy Jones <banjeremy@gmail.com>
2017-10-11 15:39:36 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+let hitCount = 0;
+app.get('/hitcount', (req, res) => {
+  res.json({ hitCount: ++hitCount });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`The party is happening at: http://localhost:${app.get('port')}/`);
+});
